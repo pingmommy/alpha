ca23c9d5bdd131aff1ccf8a77d00efc26a29ec43
mcar8764 <mcar8764@gmail.com>
2023-10-10 15:13:17 +0900

    adapt mysql

---
+++ src/main/java/com/example/mapper/CityMapper.java
@@ -0,0 +1,73 @@
+package com.example.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectProvider;
+import org.apache.ibatis.jdbc.SQL;
+
+import com.example.model.City;
+
+@Mapper
+public interface CityMapper {
+	
+	@Select("select * from city")
+	List<City> selectAll(); 
+	
+	@Select("select code as \"code\", name as \"name\"  from country")
+	//@Select("select code as code, name as name  from country")
+	// 소문자로 바꾼 이유는 EL에서 접근할 때 소문자를 주로 쓰기 때문인데, 쿼리문으로 데이터를 불러올 때는 컬럼명이 다 대문자이기 때문. 
+	// 별칭을 줄 때 쌍따옴표를 준 이유는 대소문자를 구분하려고. 
+	// 쌍따옴표 안 주면 소문자로 작성해도 대문자로 인식할 수 있다. 
+	List<Map<String, Object>> selectCountryCodes(); // key(String)이 컬럼명 value(Object)는 값.
+	
+	
+	@Select("select * from city where countryCode =#{code, jdbcType=VARCHAR}")
+	List<City> selectByCode(String code);
+
+	@SelectProvider(type = SqlProvider.class, method = "searchAll")
+	List<City> searchAll(String code,Long population);
+	public static class SqlProvider {
+		
+		public static String searchAll(String code,Long population) {
+			
+			var s = new SQL();
+			s.SELECT("*");
+			s.FROM("city");
+			
+			if(code !=null && !code.trim().equals("")) {
+				s.WHERE(String.format("countryCode='%s'", code));
+			}
+			if(population != null) {
+				
+				s.WHERE(String.format("population >= %d", population));
+			}
+			return s.toString();
+		}
+	}
+	
+	
+	
+}
+
+
+
+//select * from city;
+//
+//select * from city where country_code ='KOR';
+
+//select * from city where population >=9000000;
+//
+//select * from city where country_code ='KOR' and population >=9000000;
+
+
+//--1. 모두 조회 over3000
+//select * from city; 
+//
+//--2. 조건조회(city만 조회) 276
+//select * from city WHERE name LIKE '%A%'; 
+//
+//--3.조건조회(city를 조회하고 결과 안에서 구역을 재조회)16
+//select * from city WHERE name LIKE '%K%' and district LIKE '%M%';
---
+++ src/main/webapp/WEB-INF/city/list.jsp
@@ -0,0 +1,86 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+<%@ taglib prefix="fmt" uri="jakarta.tags.fmt" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>city</title>
+<style type="text/css">
+
+tbody > tr:nth-child(2n){
+	background: #eda56050;
+}
+
+</style>
+
+
+
+<script type="text/javascript">
+window.onload = function(){
+	population.oninput = function (e) {
+
+	    let value =parseInt(e.target.value);
+	    
+	    pop_value.innerText =value.toLocaleString();
+	    
+	}
+	
+	    pop_value.innerText =parseInt(population.value).toLocaleString();
+	
+}
+</script>
+</head>
+<body>
+<h1>CITYLIST</h1>
+<hr>
+<form action="/city/list">
+<input type="search" list="codes" name="code" value="${param.code}">
+<datalist id="codes">
+ <c:forEach var="c" items="${codes}">
+  <option value="${c.code}">[${c.code}] ${c.name}</option>
+ </c:forEach>
+</datalist>
+
+<input type="range" name="population"  value="${empty param.population? 0 : param.population}" id="population" min="0" max="10000000" step="100000">
+<label><span id="pop_value"></span>이상</label>
+
+<input type="submit" value="도시검색">
+
+</form>
+
+<hr>
+
+<table border="1" width ="500">
+<thead>
+	<tr>
+	<th>NO</th>
+	<th>id</th>
+	<th>name</th>
+	<th>countryCode</th>
+	<th>district</th>
+	<th>population</th>
+	<th>population</th>
+	
+	</tr>
+</thead>
+
+<tbody>
+ 	<c:forEach var="city" items="${list}" varStatus="status">
+ 	<tr>
+ 		<td>${status.count}</td>
+ 	 	<td>${city.id}</td>
+ 	 	<td>${city.name}</td>
+ 	 	<td>${city.countryCode}</td>
+ 	 	<td>${city.district}</td>
+ 	 	<td align="right">${city.population}</td>
+ 	 	<td align="right"><fmt:formatNumber pattern="###,###,###,####" value="${city.population}"/></td>
+ 	</tr>
+ 	</c:forEach>
+</tbody>
+</table>
+
+
+</body>
+</html>
